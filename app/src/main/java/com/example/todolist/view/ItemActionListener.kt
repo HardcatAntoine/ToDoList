2254f60234b8558d7423dd3fa0ff162a63388250
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-01-25 14:50:28 +0300

    remove method LongItemClick

--- app/src/main/java/com/example/todolist/view/ItemActionListener.kt
+++ app/src/main/java/com/example/todolist/view/ItemActionListener.kt
@@ -4,6 +4,5 @@ import android.view.View
 import com.example.todolist.data.local.Note
 
 interface ItemActionListener {
-    fun onItemLongClick(note: Note, view: View)
     fun onItemClick(note: Note)
 }
