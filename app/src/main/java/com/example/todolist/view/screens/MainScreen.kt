6c8da7cf3fc53dfa7c318a4a24f58aaf3f37808a
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-01-29 12:38:38 +0300

    AddNoteButton decomposition

--- app/src/main/java/com/example/todolist/view/screens/MainScreen.kt
+++ app/src/main/java/com/example/todolist/view/screens/MainScreen.kt
@@ -3,21 +3,11 @@ package com.example.todolist.view.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.FloatingActionButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.todolist.R
 import com.example.todolist.data.local.Note
+import com.example.todolist.view.compose_components.AddNoteButton
 import com.example.todolist.view.compose_components.NotesList
 import com.example.todolist.view.compose_components.TopBar
 import com.example.todolist.viewmodel.NotesViewModel
@@ -33,22 +23,8 @@ fun MainScreen(
             TopBar()
             NotesList(notes = notes, navController = navController, viewModel = viewModel)
         }
-        FloatingActionButton(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(24.dp),
-            onClick = { navController.navigate(R.id.action_mainFragment_to_createNoteFragment) },
-            shape = RoundedCornerShape(15.dp),
-            elevation = FloatingActionButtonDefaults.elevation(10.dp),
-            backgroundColor = MaterialTheme.colors.primarySurface
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.add_icon),
-                contentDescription = "Add"
-            )
-        }
+        AddNoteButton(boxScope = this, navController = navController)
     }
-
 }
 
 
