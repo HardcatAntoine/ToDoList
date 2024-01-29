21020b3a2fe5a3d83420e33dda8c856cb5229982
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-01-29 12:07:42 +0300

    create fun AddNoteButton

---
+++ app/src/main/java/com/example/todolist/view/compose_components/AddNoteButton.kt
@@ -0,0 +1,11 @@
+package com.example.todolist.view.compose_components
+
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.navigation.NavController
+
+@Composable
+fun AddNoteButton(alignment: Alignment, modifier: BoxScope, navController: NavController) {
+
+}
